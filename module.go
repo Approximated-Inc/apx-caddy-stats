e6adf05babe0a0fdf9fe2e02b4d0ac15b0ea9fe6
Carter Bryden <carter.bryden@7sage.com>
2026-07-28 19:56:16 -0700

    feat(form): register form endpoint + protection modules

--- module.go
+++ module.go
@@ -4,4 +4,6 @@ import "github.com/caddyserver/caddy/v2"
 
 func init() {
 	caddy.RegisterModule(&ChallengeHandler{})
+	caddy.RegisterModule(&FormEndpointHandler{})
+	caddy.RegisterModule(&FormProtectionHandler{})
 }
